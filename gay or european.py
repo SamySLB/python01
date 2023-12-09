6a0de0b23d7baca6e306ad71178ea013e8042171
Samara Barcelos de Lima <130487962+SamySLB@users.noreply.github.com>
2023-12-09 18:45:20 -0300

    Add files via upload

---
+++ gay or european.py
@@ -0,0 +1,9 @@
+name = input ('qual sua sexualidade?')
+if name == "gay" :
+    print ("senti o cheiro de longe ^~^")
+elif name == "lésbica" :
+    print ('hmm, oi casada *u*')
+elif name == "bissexual" :
+    print( 'tem o poder absoluto em suas mãos')
+else: print ('bom pra você')
+
